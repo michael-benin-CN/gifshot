cf328d34b5b55fbc70747e34d2465df8455d9eb0
Chase West <chaserit@gmail.com>
2014-05-15 22:22:10 -0700

    Adding gif codec

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,9 +1,9 @@
 // gifshot.js
-define(['utils', 'videoStream'], function(util, videoStream) {
+define(['utils', 'videoStream', 'gifCodec'], function(util, videoStream, gifCodec) {
 	videoStream.startVideoStreaming(function(cameraStream, videoElement, width, height) {
 
-
-		var images = [],
+		var codecWriter,
+				images = [],
 				i = 0,
 				canvas,
 				img;
@@ -24,23 +24,29 @@ define(['utils', 'videoStream'], function(util, videoStream) {
 
 		function snapShot(){
 
+			var imageData;
+
 			if (cameraStream) {
 				canvas.width = videoElement.videoWidth;
 				canvas.height = videoElement.videoHeight;
-				context.drawImage(videoElement, 0, 0, videoElement.videoWidth, videoElement.videoHeight);
-				console.log(context.getImageData(0, 0, videoElement.videoWidth, videoElement.videoHeight));
-				images.push(canvas.toDataURL('image/gif'));
+
+				imageData = context.getImageData(0, 0, videoElement.videoWidth, videoElement.videoHeight);
+				images.push(imageData);
+
+				codec = new gifCodec.GifWriter(imageData, videoElement.videoWidth, videoElement.videoHeight);
+				console.log(codec);
+
 			}
 
 		}
 
-		setInterval(function(){
+		/*setInterval(function(){
 			if(images.length){
 					var next = ++i;
 					i = images.length && next < images.length ? i : 0;
 					img.src = images[i];
 				}
-		}, 100);
+		}, 100);*/
 
 	});
 });
