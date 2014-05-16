ad870b18ab33f45c8b7652f6825a312c628b63ed
gfranko <gregfranko5@gmail.com>
2014-05-16 02:12:33 -0700

    Semi-working prototype

---
+++ src/modules/screenShot.js
@@ -0,0 +1,91 @@
+define(['utils'], function(utils) {
+	return {
+    	getWebcamGif: function (obj, callback) {
+    		callback = utils.isFunction(callback) ? callback : function() {}
+			if(!utils.isCanvasSupported()){
+				utils.log('ERROR: Canvas not supported');
+				callback();
+				return;
+			}
+
+			var canvas = document.createElement('canvas'),
+				context,
+				videoElement = obj.videoElement,
+				gifWidth = obj.gifWidth,
+				gifHeight = obj.gifHeight,
+				videoWidth = obj.videoWidth,
+				videoHeight = obj.videoHeight,
+				crop = obj.crop,
+				interval = obj.interval,
+				progressCallback = obj.progressCallback,
+				numFrames = obj.numFrames,
+				pendingFrames = numFrames,
+				ag = new Animated_GIF({
+					workerPath: 'src/vendor/Animated_GIF.worker.js'
+				}),
+				sourceX = Math.floor(crop.scaledWidth / 2),
+				sourceWidth = videoWidth - crop.scaledWidth,
+				sourceY = Math.floor(crop.scaledHeight / 2),
+				sourceHeight = videoHeight - crop.scaledHeight,
+				captureFrame = function() {
+					var framesLeft = pendingFrames - 1;
+					if (framesLeft > 0) {
+					  setTimeout(captureFrame, interval * 1000); // timeouts are in milliseconds
+					}
+
+					context.drawImage(videoElement,
+					  sourceX, sourceY, sourceWidth, sourceHeight,
+					  0, 0, gifWidth, gifHeight);
+
+					ag.addFrameImageData(context.getImageData(0, 0, gifWidth, gifHeight));
+					pendingFrames = framesLeft;
+
+					// Call back with an r value indicating how far along we are in capture
+					progressCallback((numFrames - pendingFrames) / numFrames);
+
+					if (!pendingFrames) {
+					  ag.getBase64GIF(function(image) {
+					    // Ensure workers are freed-so we avoid bug #103
+					    // https://github.com/meatspaces/meatspace-chat/issues/103
+					    ag.destroy();
+					    callback(image);
+					  });
+					}
+				};
+
+			numFrames = numFrames !== undefined ? numFrames : 3;
+			interval = interval !== undefined ? interval : 0.1; // In seconds
+
+			canvas.width = gifWidth;
+			canvas.height = gifHeight;
+			context = canvas.getContext('2d');
+
+			ag.setSize(gifWidth, gifHeight);
+			ag.setDelay(interval);
+
+			captureFrame();
+		},
+		'getCropDimensions': function (obj) {
+		    var width = obj.videoWidth,
+				height = obj.videoHeight,
+				gifWidth = obj.gifWidth,
+				gifHeight = obj.gifHeight,
+				result = {
+			    	width: 0,
+			    	height: 0,
+			    	scaledWidth: 0,
+			    	scaledHeight: 0
+		    	};
+
+		    if (width > height) {
+		      result.width = Math.round(width * (gifHeight / height)) - gifWidth;
+		      result.scaledWidth = Math.round(result.width * (height / gifHeight));
+		    } else {
+		      result.height = Math.round(height * (gifWidth / width)) - gifHeight;
+		      result.scaledHeight = Math.round(result.height * (width / gifWidth));
+		    }
+
+		    return result;
+  		}
+	};
+});
--- src/modules/utils.js
+++ src/modules/utils.js
@@ -32,6 +32,12 @@ define({
         }
         return Object.prototype.toString.call(func) === '[object Function]';
     },
+    'isElement': function(elem) {
+        return elem && elem.nodeType === 1;
+    },
+    'isString': function(value) {
+        return typeof value === 'string' || Object.prototype.toString.call(value) === '[object String]';
+    },
     'isCanvasSupported': function() {
       var el = document.createElement('canvas');
       return !!(el.getContext && el.getContext('2d'));
@@ -45,5 +51,47 @@ define({
             console.log.apply(window.console, arguments);
         }
     },
-    'noop': function() {}
+    'noop': function() {},
+    'each': function(collection, callback) {
+        var x, len;
+        if(this.isArray(collection)) {
+            x = -1;
+            len = collection.length;
+            while(++x < len) {
+                if (callback(x, collection[x]) === false) {
+                    break;
+                }
+            }
+        } else if(this.isObject(collection)) {
+            for(x in collection) {
+                if(collection.hasOwnProperty(x)) {
+                    if (callback(x, collection[x]) === false) {
+                        break;
+                    }
+                }
+            }
+        }
+    },
+    'mergeOptions': function (defaultOptions, userOptions) {
+        for(var i=1; i<arguments.length; i++) {
+            for(var key in arguments[i]) {
+                if(arguments[i].hasOwnProperty(key)) {
+                    arguments[0][key] = arguments[i][key];
+                }
+            }
+        }
+        return arguments[0];
+    },
+    'setCSSAttr': function(elem, attr, val) {
+        if(!this.isElement(elem)) {
+            return;
+        }
+        if(this.isString(attr) && this.isString(val)) {
+            elem.style[attr] = val;
+        } else if(this.isObject(attr)) {
+            this.each(attr, function(key, val) {
+                elem.style[key] = val;
+            });
+        }
+    }
 });
