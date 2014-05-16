2086f6832bf37264dadec8f1f5eeb4a7297dc10f
Chase West <chaserit@gmail.com>
2014-05-15 18:24:46 -0700

    Stashed changes

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,9 +1,13 @@
 // gifshot.js
 define(['videoStream'], function(videoStream) {
 	videoStream.startVideoStreaming(function(cameraStream, videoElement, width, height) {
-		console.log('cameraStream', cameraStream);
-		console.log('videoElement', videoElement);
-		console.log('width', width);
-		console.log('height', height);
+
+		videoElement.onclick = function(){
+			console.log(this);
+		};
+
+		document.body.appendChild(videoElement);
+
+
 	});
 });
