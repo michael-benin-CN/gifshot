021b69326533ef8aac6ccb0ecf8c51f5fcdf5970
Greg Franko <gregfranko5@gmail.com>
2014-09-26 13:56:21 -0700

    Updated homepage

---
+++ js/randomizeGIFs.js
@@ -0,0 +1,26 @@
+(function() {
+  var POSSIBLE_IMAGES = 21,
+    TOTAL_IMAGES = 9,
+    COLUMNS = 3,
+    randomNums = _.shuffle(_.range(1, POSSIBLE_IMAGES)).slice(0, TOTAL_IMAGES),
+    isEven = function(num) {
+      return num % 2 === 0;
+    };
+
+  _.each(randomNums, function(num, iterator) {
+    iterator += 1;
+    var GIF = document.createElement('img'),
+      rowNum = Math.ceil(iterator/COLUMNS),
+      rowElem = document.querySelector('.gifshot-images .row-' + rowNum);
+
+    GIF.classList.add('hl');
+    // Make sure to place the gifshot GIF in the middle every time
+    if(iterator === 5) {
+      GIF.src = 'images/gifshot.gif';
+    } else {
+      GIF.src = 'images/gifshot-' + num + '.gif';
+    }
+
+    rowElem.appendChild(GIF);
+  });
+}());
