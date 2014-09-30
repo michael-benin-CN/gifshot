806ca11a0da68e85925f48776e2474b29df887e7
gfranko <gfranko@yahoo-inc.com>
2014-09-30 04:24:51 -0700

    Added image loading logic to try and stop broken images on the homepage from happening

--- js/randomizeGIFs.js
+++ js/randomizeGIFs.js
@@ -2,7 +2,10 @@
   var POSSIBLE_IMAGES = 26,
     TOTAL_IMAGES = 9,
     COLUMNS = 3,
-    randomNums = _.shuffle(_.range(1, POSSIBLE_IMAGES + 1)).slice(0, TOTAL_IMAGES);
+    randomNums = _.shuffle(_.range(1, POSSIBLE_IMAGES + 1)).slice(0, TOTAL_IMAGES),
+    loadedImages = 0,
+    gifshotLoader = document.querySelector('.gifshot-loader');
+    imageRows = Array.prototype.slice.call(document.querySelectorAll('.rows-container'));
 
   _.each(randomNums, function(num, iterator) {
     iterator += 1;
@@ -17,6 +20,16 @@
     } else {
       GIF.src = 'images/gifshot-' + num + '.gif';
     }
+
+    GIF.onload = function() {
+      loadedImages += 1;
+      if(loadedImages >= TOTAL_IMAGES) {
+        gifshotLoader.classList.add('hidden');
+        imageRows.forEach(function(currentImageRow) {
+          currentImageRow.classList.remove('hidden');
+        });
+      }
+    };
 
     rowElem.appendChild(GIF);
   });
